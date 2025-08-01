570ff4c02a40f40d104caba94d1cb3c43ac77175
MatiasGGallardo <matiasgallardo196@gmail.com>
2025-08-01 15:14:59 +1000

    Remove dist folder from version control

--- back/dist/interfaces/IAppointment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppointmentStatus = void 0;
-var AppointmentStatus;
-(function (AppointmentStatus) {
-    AppointmentStatus["Active"] = "active";
-    AppointmentStatus["Cancelled"] = "cancelled";
-})(AppointmentStatus = exports.AppointmentStatus || (exports.AppointmentStatus = {}));
--- back/dist/utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchAsync = void 0;
-const catchAsync = (controller) => {
-    return (req, res, next) => {
-        controller(req, res, next).catch((err) => next(err));
-    };
-};
-exports.catchAsync = catchAsync;
