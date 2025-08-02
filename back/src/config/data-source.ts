cb703b9f73d6c6fcab2958b5d779ab49f4fa35aa
MatiasGGallardo <matiasgallardo196@gmail.com>
2025-08-02 15:04:37 +1000

    Refactor data source configuration to support production environment with DATABASE_URL

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -1,22 +1,31 @@
 import { DataSource } from "typeorm";
-import { DB, HOST, PASSDB, PORTDB, USERNAMEDB } from "./envs";
+import { DATABASE_URL, DB, HOST, PASSDB, PORTDB, USERNAMEDB } from "./envs";
 import { User } from "../entities/User";
 import { Credential } from "../entities/Credential";
 import { Appointment } from "../entities/Appointment";
 
+const isProduction = DATABASE_URL;
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: HOST,
-    port: PORTDB,
-    username: USERNAMEDB,
-    password: PASSDB,
-    database: DB,
-    synchronize: true,
-    dropSchema: false,
-    logging: false,
-    entities: [User,Credential,Appointment],
-    subscribers: [],
-    migrations: [],
-})
-
+  type: "postgres",
+  ...(isProduction
+    ? {
+        url: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: HOST,
+        port: PORTDB,
+        username: USERNAMEDB,
+        password: PASSDB,
+        database: DB,
+      }),
+  synchronize: true,
+  dropSchema: false,
+  logging: false,
+  entities: [User, Credential, Appointment],
+  subscribers: [],
+  migrations: [],
+});
--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -1,13 +1,14 @@
-import "dotenv/config"
+import "dotenv/config";
 
-export const PORT=process.env.PORT
-export const HOST=process.env.HOST
-export const PORTDB=Number(process.env.PORTDB)
-export const USERNAMEDB=process.env.USERNAMEDB
-export const PASSDB=process.env.PASSDB
-export const DB=process.env.DB
-export const SALT_ROUNDS=process.env.SALT_ROUNDS
-export const JWT_SECRET=process.env.JWT_SECRET
-export const ENVIRONMENT = process.env.ENVIRONMENT === "development" 
-export const SENDGRID_API_KEY= process.env.SENDGRID_API_KEY
-export const EMAIL_FROM=process.env.EMAIL_FROM
+export const DATABASE_URL = !!process.env.DATABASE_URL;
+export const PORT = process.env.PORT;
+export const HOST = process.env.HOST;
+export const PORTDB = Number(process.env.PORTDB);
+export const USERNAMEDB = process.env.USERNAMEDB;
+export const PASSDB = process.env.PASSDB;
+export const DB = process.env.DB;
+export const SALT_ROUNDS = process.env.SALT_ROUNDS;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const ENVIRONMENT = process.env.ENVIRONMENT === "development";
+export const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+export const EMAIL_FROM = process.env.EMAIL_FROM;
